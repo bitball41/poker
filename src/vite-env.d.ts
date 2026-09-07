57d49c40796347e078bf0a76d72a8fec3abd3bbf
Conno <nissimconnor@gmail.com>
2026-09-07 10:19:16 -0700

    Declare Liminal parent origin override

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -5,6 +5,7 @@ interface ImportMetaEnv {
   readonly VITE_SUPABASE_ANON_KEY: string;
   readonly VITE_LIMINAL_SUPABASE_URL?: string;
   readonly VITE_LIMINAL_SUPABASE_KEY?: string;
+  readonly VITE_LIMINAL_PARENT_ORIGIN?: string;
 }
 
 interface ImportMeta {
